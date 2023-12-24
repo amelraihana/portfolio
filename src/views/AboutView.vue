69f7477186c7492ec3a2f369785d4527fc7c1368
Amelia Raihana <91269801+amelraihana@users.noreply.github.com>
2023-12-24 15:26:46 +0800

    💎Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -127,7 +127,7 @@ export default {
         class="text-sm md:text-lg text-justify flex flex-col gap-4 md:flex-row md:gap-8 md:justify-left md:items-center">
         <div class="flex justify-center">
           <img class="w-9/12 rounded-full mb-3 fadein-up"
-             src="/Users/amelia/portfolio/public/amelia.png" alt="ame">
+             src="/amelia.png" alt="ame">
         </div>
         <div class="md:w-11/12">
           <p class="mb-3 md:mb-7 fadein-left fadeins-1">
